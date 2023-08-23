e12cd3081958788e08c2248095cf3f71d1d251d7
ManishJoshi <joshi8763@gmail.com>
2023-08-24 00:00:51 +0530

    complete project

--- mydb.py
+++ mydb.py
@@ -1,12 +1,22 @@
-import mysql.connector
-dataBase=mysql.connector.connect(
-    host='localhost',
-    user='root',
-    passwd='admin'
+# Install Mysql on your computer
+# https://dev.mysql.com/downloads/installer/
+# pip install mysql
+# pip install mysql-connector
+# pip install mysql-connector-python 
 
-)
-#prepare a cursopr object
-cursorObject=dataBase.cursor()
-#create a database
-cursorObject.execute("CREATE DATABASE elderco")
-print("All Done")
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+	host = 'localhost',
+	user = 'root',
+	passwd = 'admin'
+
+	)
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# Create a database
+cursorObject.execute("CREATE DATABASE JoshiDb")
+
+print("All Done!")
--- website/models.py
+++ website/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True)
+    first_name=models.CharField(max_length=20)
+    last_name=models.CharField(max_length=20)
+    email=models.CharField(max_length=20)
+    address=models.CharField(max_length=20)
+    city=models.CharField(max_length=20)
+    state=models.CharField(max_length=20)
+    Zipcode=models.CharField(max_length=20)
+
+    def _str_(self):
+        return(f"{self.first_name}){self.last_name}")
+
+    
+
+
